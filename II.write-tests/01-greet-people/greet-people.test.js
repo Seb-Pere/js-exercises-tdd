1a1cd87deaf974ab4af6f8a06e3a0649dda925e2
Sébastien Pereira <Seb@MacBook-Pro-de-Sebastien-2.local>
2021-03-05 09:10:03 +0100

    correction

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,21 @@
-test("print list of names prefixed with Hello", function() {
-  // Arrange
-  // Act
-  // Assert
+const greetPeople = require("./greet-people");
+
+test("It should return a list of names prefixed with Hello", function() {
+  const output = greetPeople(['Irina', 'Ashleigh', 'Etza'])
+  const expected = 'Hello IrinaAshleighEtza'
+
+  expect(output).toEqual(expected)
 });
+
+test('It should return a list of names prefixed with Hello', function() {
+  const output = greetPeople(['a', 'b', 'c'])
+  const expected = 'Hello abc'
+
+  expect(output).toEqual(expected)
+})
+
+test('It should throw a TypeError if the parameter is not an array', function() {
+  expect(
+    () => { greetPeople(42) }
+  ).toThrowError(new TypeError('people.forEach is not a function'))
+})
