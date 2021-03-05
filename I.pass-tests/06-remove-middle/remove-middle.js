b9962550328bb4aa0b3f3f4be1b7822847ed051c
Sébastien Pereira <Seb@MacBook-Pro-de-Sebastien-2.local>
2021-03-05 09:08:43 +0100

    correction

---
+++ I.pass-tests/06-remove-middle/remove-middle.js
@@ -0,0 +1,6 @@
+module.exports = (words) => {
+    const iMiddleWord = Math.floor(words.length / 2)
+    
+    return words.splice(iMiddleWord, 1)
+  }
+  
---
+++ I.pass-tests/09-car-sales/car-sales.js
@@ -0,0 +1,16 @@
+module.exports = (carsSold) => {
+    const totals = {}
+  
+    carsSold.forEach(
+      ({ make, price }) => {
+        if (!totals[make]) {
+          totals[make] = 0
+        }
+  
+        totals[make] += price
+      },
+    )
+  
+    return totals
+  }
+  
